a002424c315b95d2dbac239e73920e324c43843c
Mayank143 <chhotachetan8@gmail.com>
2016-11-11 21:45:25 +0530

    add files via upload
    some other files of cms

---
+++ APPOINTMENT.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class APPOINTMENT : System.Web.UI.Page
+{
+    Class1 c = new Class1();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void submit_Click(object sender, EventArgs e)
+    {
+        string query="insert into registration values('"+id.Text+"','"+name.Text+"','"+fname.Text+"','"+Address.Text+"','"+Mobile.Text+"','"+Email.Text+"','"+reason.Text+"')";
+        c.ins_up_del(query);
+
+    }
+}
